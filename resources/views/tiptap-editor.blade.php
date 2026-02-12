85d13a5bce7b490dad432fad46eb4cc1625483f2
Alessandro Ferrini <madferro@gmail.com>
2026-02-12 14:19:08 +0100

    fix: replace ax-load with x-load for Filament 4 compatibility

--- resources/views/tiptap-editor.blade.php
+++ resources/views/tiptap-editor.blade.php
@@ -30,9 +30,8 @@
             >
                 <div
                     wire:ignore
-                    x-ignore
-                    ax-load="visible"
-                    ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('tiptap', 'awcodes/tiptap-editor') }}"
+                    x-load
+                    x-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('tiptap', 'awcodes/tiptap-editor') }}"
                     class="relative z-0 tiptap-wrapper rounded-md bg-white dark:bg-gray-900 focus-within:ring focus-within:ring-primary-500 focus-within:z-10"
                     x-bind:class="{ 'tiptap-fullscreen': fullScreenMode }"
                     x-data="tiptap({
